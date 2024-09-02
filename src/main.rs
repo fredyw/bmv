aad9d9976914395ff462c8b8f34d280a9e067f26
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-02 16:53:33 -0500

    Add command line args parsing code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+extern crate clap;
+
+use std::path::PathBuf;
+use std::process;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Set the directory to perform a bulk rename.
+    #[arg(short, long)]
+    dir: PathBuf,
+
+    /// Set the from regex pattern.
+    #[arg(short, long)]
+    from: String,
+
+    /// Set the to regex pattern.
+    #[arg(short, long)]
+    to: String,
+
+    /// Perform a dry-run.
+    #[arg(short = 'r', long, default_value_t = false)]
+    dry_run: bool,
+}
+
+fn err_and_exit(message: &str) {
+    eprintln!("Error: {}", message);
+    process::exit(1);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    if !args.dir.is_dir() {
+        err_and_exit(&format!("{:?} is not a directory.", args.dir.as_path()));
+    }
 }
